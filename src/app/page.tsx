f515f6591f57246a8f05cb1e979e87cf06e64476
IlijaRad <ilija.radivojevic237@gmail.com>
2025-09-02 14:38:10 +0200

    Add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,13 @@
+import { logos, navigation } from "@/lib/data/static";
+import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-const navigation = [
-  {
-    id: 1,
-    label: "Vehicles List",
-    href: "/vehicles",
-  },
-  {
-    id: 2,
-    label: "About Us",
-    href: "/about",
-  },
-  {
-    id: 3,
-    label: "Services",
-    href: "/services",
-  },
-  {
-    id: 4,
-    label: "Contact Us",
-    href: "/contact",
-  },
-];
-
-const logos = [
-  {
-    id: 1,
-    src: "/home-page/logo1.png",
-    make: "maserati",
-  },
-  {
-    id: 2,
-    src: "/home-page/logo2.png",
-    make: "ford",
-  },
-  {
-    id: 3,
-    src: "/home-page/logo3.png",
-    make: "bmw",
-  },
-  {
-    id: 4,
-    src: "/home-page/logo4.png",
-    make: "audi",
-  },
-  {
-    id: 5,
-    src: "/home-page/logo5.png",
-    make: "kia",
-  },
-];
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "This is the home page description",
+  keywords: "These, are, the, home, page, keywords",
+};
 
 export default function Page() {
   return (
